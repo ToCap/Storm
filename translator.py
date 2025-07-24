65e75dc1341ec9ef8a84be66959e5f9cba7abdc0
Tocap <thomas.barbillon@gmail.com>
2025-07-24 16:34:23 +0200

    fix add missing license information

--- translator.py
+++ translator.py
@@ -1,3 +1,17 @@
+# ***************************************************************************************************
+# Storm -  Smart Telemetry & Operational Recording for Mindstorm https://github.com/ToCap/Storm
+#
+# Copyright (C) 2025  GC-MS Software (ThomTheCat)
+#
+# This source code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation; See <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>.
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+# License for the specific language governing rights and limitations
+# under the License.
+# ***************************************************************************************************
 
 import sys
 
